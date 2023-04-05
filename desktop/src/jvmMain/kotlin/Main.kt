ad27c6c8774dc974af4c81a293b8c599bff8c9f8
ubertrombone <josh_rose@alumni.baylor.edu>
2023-04-05 21:41:35 +0300

    Prepares Desktop module

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,10 +1,29 @@
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.joshrose.common.App
-
+import com.roseFinancials.lenafx.ui.theme.PlotsForComposeTheme
+import java.awt.Dimension
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    val windowParams = DpSize(1250.dp, 750.dp)
+    val windowState = rememberWindowState(size = windowParams)
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = windowState,
+        resizable = true,
+        title = "PlotsForKotlin"
+    ) {
+        window.minimumSize = Dimension(
+            windowParams.width.value.toInt(),
+            windowParams.height.value.toInt()
+        )
+
+        PlotsForComposeTheme {
+            App()
+        }
     }
 }
