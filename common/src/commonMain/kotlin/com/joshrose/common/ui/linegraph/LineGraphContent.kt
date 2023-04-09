c8fd2acb687f8501dd6801f26043ac89bb2a638d
ubertrombone <josh_rose@alumni.baylor.edu>
2023-04-09 13:09:46 +0300

    Removes background

--- common/src/commonMain/kotlin/com/joshrose/common/ui/linegraph/LineGraphContent.kt
+++ common/src/commonMain/kotlin/com/joshrose/common/ui/linegraph/LineGraphContent.kt
@@ -1,13 +1,11 @@
 package com.joshrose.common.ui.linegraph
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.joshrose.common.linegraph.LineGraphComponent
 
 @Composable
@@ -16,7 +14,7 @@ fun LineGraphContent(
     modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = modifier.fillMaxSize().background(Color.Gray),
+        modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Text(text = "This is where my graph would go")
