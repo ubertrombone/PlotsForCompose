f7e758a04c69200623fde7bbcc05118bf69ed57b
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-06 21:43:49 +0300

    Migrates Modifiers.kt repeatingClickable to awaitEachGesture.

--- app/common/src/commonMain/kotlin/com/joshrose/common/util/Modifiers.kt
+++ app/common/src/commonMain/kotlin/com/joshrose/common/util/Modifiers.kt
@@ -1,9 +1,7 @@
-@file:Suppress("DEPRECATION")
-
 package com.joshrose.common.util
 
+import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.runtime.getValue
@@ -11,9 +9,7 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.input.pointer.pointerInput
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 fun Modifier.repeatingClickable(
     interactionSource: InteractionSource,
@@ -23,20 +19,17 @@ fun Modifier.repeatingClickable(
     val currentClickListener by rememberUpdatedState(onClick)
 
     pointerInput(interactionSource, enabled) {
-        forEachGesture {
-            coroutineScope {
-                awaitPointerEventScope {
-                    val down = awaitFirstDown(requireUnconsumed = false)
-                    val heldButtonJob = launch {
-                        while (enabled && down.pressed) {
-                            currentClickListener()
-                            delay(100)
-                        }
-                    }
-                    waitForUpOrCancellation()
-                    heldButtonJob.cancel()
+        awaitEachGesture {
+            val down = awaitFirstDown(requireUnconsumed = false)
+            val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+            val heldButtonJob = scope.launch {
+                while (enabled && down.pressed) {
+                    currentClickListener()
+                    delay(100)
                 }
             }
+            waitForUpOrCancellation()
+            heldButtonJob.cancel()
         }
     }
 }
