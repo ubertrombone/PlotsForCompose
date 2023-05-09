f92c375c5543b21d85dbb4e3b7675b029ce1f77f
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-09 21:17:37 +0300

    Adds AxisPosition assets.

--- app/common/src/androidMain/kotlin/com/joshrose/common/util/CreatePainter.kt
+++ app/common/src/androidMain/kotlin/com/joshrose/common/util/CreatePainter.kt
@@ -4,14 +4,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import com.joshrose.common.R
+import com.joshrose.common.util.ImageResources.*
 
 @Composable
 internal actual fun createPainter(file: ImageResources): Painter =
     painterResource(
         when (file) {
-            ImageResources.GUIDELINES -> R.drawable.grid_on
-            ImageResources.LABELS -> R.drawable.label
-            ImageResources.AXIS_LINES -> R.drawable.line_axis
-            ImageResources.VISIBILITY -> R.drawable.visibility
+            GUIDELINES -> R.drawable.grid_on
+            LABELS -> R.drawable.label
+            AXIS_LINES -> R.drawable.line_axis
+            VISIBILITY -> R.drawable.visibility
+            ALIGN_LEFT -> R.drawable.align_horizontal_left
+            ALIGN_RIGHT -> R.drawable.align_horizontal_right
+            ALIGN_CENTER_HORIZONTAL -> R.drawable.align_horizontal_center
+            ALIGN_TOP -> R.drawable.align_vertical_top
+            ALIGN_BOTTOM -> R.drawable.align_vertical_bottom
+            ALIGN_CENTER_VERTICAL -> R.drawable.align_vertical_center
+            ALIGN_AUTO -> R.drawable.auto_awesome
         }
     )
--- app/common/src/commonMain/kotlin/com/joshrose/common/util/ImageResources.kt
+++ app/common/src/commonMain/kotlin/com/joshrose/common/util/ImageResources.kt
@@ -4,5 +4,12 @@ enum class ImageResources(val path: String) {
     GUIDELINES("grid_on.xml"),
     LABELS("label.xml"),
     AXIS_LINES("line_axis.xml"),
-    VISIBILITY("visibility.xml")
+    VISIBILITY("visibility.xml"),
+    ALIGN_LEFT("align_horizontal_left.xml"),
+    ALIGN_RIGHT("align_horizontal_right.xml"),
+    ALIGN_CENTER_HORIZONTAL("align_horizontal_center.xml"),
+    ALIGN_TOP("align_vertical_top.xml"),
+    ALIGN_BOTTOM("align_vertical_bottom.xml"),
+    ALIGN_CENTER_VERTICAL("align_vertical_center.xml"),
+    ALIGN_AUTO("auto_awesome.xml")
 }
