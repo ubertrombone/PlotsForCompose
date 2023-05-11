b9c0f5a9311be765f2a327889182f35120e9c160
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-11 19:47:18 +0300

    Adjusts AlignmentButton color scheme in light theme.

--- app/common/src/commonMain/kotlin/com/joshrose/common/ui/axes/axisline/AlignmentButton.kt
+++ app/common/src/commonMain/kotlin/com/joshrose/common/ui/axes/axisline/AlignmentButton.kt
@@ -1,5 +1,6 @@
 package com.joshrose.common.ui.axes.axisline
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
@@ -18,11 +19,14 @@ fun AlignmentButton(
     modifier: Modifier = Modifier,
     onClick: (AxisPosition?) -> Unit,
 ) {
+    val contentColor = if (selected) {
+        if (isSystemInDarkTheme()) colorScheme.primary else colorScheme.primaryContainer
+    } else {
+        if (isSystemInDarkTheme()) colorScheme.primaryContainer else colorScheme.primary
+    }
     IconButton(
         modifier = modifier,
-        colors = IconButtonDefaults.iconButtonColors(
-            contentColor = if (selected) colorScheme.primary else colorScheme.secondary
-        ),
+        colors = IconButtonDefaults.iconButtonColors(contentColor = contentColor),
         onClick = { onClick(axisPosition) }
     ) {
         Icon(painter = icon, contentDescription = contentDescription)
