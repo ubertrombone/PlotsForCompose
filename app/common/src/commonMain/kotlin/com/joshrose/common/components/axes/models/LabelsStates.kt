fdbe11ca8e2a8661bd1c829c5f77c4136b82a369
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-13 14:29:34 +0300

    Adds Reset functions.

--- app/common/src/commonMain/kotlin/com/joshrose/common/components/axes/models/LabelsStates.kt
+++ app/common/src/commonMain/kotlin/com/joshrose/common/components/axes/models/LabelsStates.kt
@@ -8,8 +8,8 @@ import com.joshrose.plotsforcompose.axis.config.util.Multiplier
 data class LabelsStates(
     val rotation: Float = 0f,
     val axisOffset: Float = 20f,
-    val rangeAdjustment: Multiplier = Multiplier(.1f),
-    val minValueAdjustment: Multiplier = Multiplier(.1f),
-    val maxValueAdjustment: Multiplier = Multiplier(.1f),
+    val rangeAdjustment: Multiplier = Multiplier(0f),
+    val minValueAdjustment: Multiplier = Multiplier(0f),
+    val maxValueAdjustment: Multiplier = Multiplier(.0f),
     val breaks: Int = 5
 ): Parcelable
