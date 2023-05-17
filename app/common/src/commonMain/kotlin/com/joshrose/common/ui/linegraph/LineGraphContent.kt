c7809241c83be0417a4f597bce7c789831548e64
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-17 21:03:01 +0300

    Prepares LineGraph.

--- app/common/src/commonMain/kotlin/com/joshrose/common/ui/linegraph/LineGraphContent.kt
+++ app/common/src/commonMain/kotlin/com/joshrose/common/ui/linegraph/LineGraphContent.kt
@@ -1,22 +1,63 @@
 package com.joshrose.common.ui.linegraph
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.joshrose.common.components.graph.LineGraphComponent
+import com.joshrose.common.util.ScrollLazyColumn
+import com.joshrose.plotsforcompose.axis.config.ContinuousAxisConfigDefaults
+import com.joshrose.plotsforcompose.axis.config.axisline.AxisLineConfigDefaults
+import com.joshrose.plotsforcompose.axis.config.guidelines.GuidelinesConfigDefaults
+import com.joshrose.plotsforcompose.axis.config.labels.ContinuousLabelsConfigDefaults
+import com.joshrose.plotsforcompose.axis.config.util.Multiplier
+import com.joshrose.plotsforcompose.linegraph.LineGraph
+import com.joshrose.plotsforcompose.linegraph.model.Coordinates.IntData
 
 @Composable
 fun LineGraphContent(
     component: LineGraphComponent,
     modifier: Modifier = Modifier
 ) {
+
+    val yConfig = ContinuousAxisConfigDefaults.continuousAxisConfigDefaults()
+        .copy(
+            showGuidelines = true,
+            guidelines = GuidelinesConfigDefaults.guidelinesConfigDefaults().copy(
+                lineColor = MaterialTheme.colorScheme.onBackground,
+                alpha = Multiplier(.5f),
+                padding = 0f
+            ),
+            labels = ContinuousLabelsConfigDefaults.continuousLabelsConfigDefaults().copy(
+                axisOffset = 20.dp,
+                fontColor = MaterialTheme.colorScheme.primary
+            ),
+            axisLine = AxisLineConfigDefaults.axisLineConfigDefaults().copy(
+                lineColor = MaterialTheme.colorScheme.primary,
+            )
+        )
+
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "This is where my graph would go")
+        ScrollLazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+        ) {
+            item {
+                LineGraph(
+                    data = listOf(IntData(1, 1f), IntData(4, 4f), IntData(3, 3f), IntData(11, 11f)),
+                    yAxisConfig = yConfig,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                        .padding(50.dp)
+                )
+            }
+        }
     }
 }
--- plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/linegraph/LineGraph.kt
+++ plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/linegraph/LineGraph.kt
@@ -3,28 +3,43 @@ package com.joshrose.plotsforcompose.linegraph
 import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.rememberTextMeasurer
 import com.joshrose.plotsforcompose.axis.config.ContinuousAxisConfig
 import com.joshrose.plotsforcompose.axis.config.ContinuousAxisConfigDefaults
+import com.joshrose.plotsforcompose.axis.util.AxisPosition.Companion.toXAxisPosition
+import com.joshrose.plotsforcompose.axis.util.AxisPosition.Companion.toYAxisPosition
+import com.joshrose.plotsforcompose.axis.util.AxisPosition.XAxisPosition
+import com.joshrose.plotsforcompose.axis.util.AxisPosition.YAxisPosition
+import com.joshrose.plotsforcompose.axis.util.Range
 import com.joshrose.plotsforcompose.axis.util.floatLabels
+import com.joshrose.plotsforcompose.axis.y.continuous.continuousYAxis
 import com.joshrose.plotsforcompose.common.maxValue
 import com.joshrose.plotsforcompose.common.minValue
 import com.joshrose.plotsforcompose.common.range
 import com.joshrose.plotsforcompose.exception.InvalidRangeException
 import com.joshrose.plotsforcompose.linegraph.config.LineGraphConfig
 import com.joshrose.plotsforcompose.linegraph.config.LineGraphConfigDefaults
-import com.joshrose.plotsforcompose.linegraph.model.*
+import com.joshrose.plotsforcompose.linegraph.model.Coordinates
+import com.joshrose.plotsforcompose.linegraph.model.Coordinates.Companion.firstXValue
+import com.joshrose.plotsforcompose.linegraph.model.Coordinates.Companion.lastXValue
+import com.joshrose.plotsforcompose.linegraph.model.Coordinates.Companion.maxYValue
+import com.joshrose.plotsforcompose.linegraph.model.Coordinates.Companion.minYValue
 import com.joshrose.plotsforcompose.util.LoadingState
 import com.joshrose.plotsforcompose.util.LoadingState.*
 
+// TODO: User should be able to only draw a subset of the labels leaving the rest as guidelines, etc.
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun LineGraph(
-    data: List<LineData>,
+    data: List<Coordinates<*>>,
     modifier: Modifier = Modifier,
     xAxisConfig: ContinuousAxisConfig = ContinuousAxisConfigDefaults.continuousAxisConfigDefaults(),
     yAxisConfig: ContinuousAxisConfig = ContinuousAxisConfigDefaults.continuousAxisConfigDefaults(),
     lineGraphConfig: LineGraphConfig = LineGraphConfigDefaults.lineGraphConfigDefaults()
 ) {
     var state by remember { mutableStateOf<LoadingState>(Success) }
+    val yTextMeasurer = rememberTextMeasurer()
 
     if (data.isEmpty()) state = Empty
 
@@ -33,6 +48,8 @@ fun LineGraph(
     val maxYValue by remember { derivedStateOf { maxValue(data.maxYValue(), yAxisConfig.labels.maxValueAdjustment) } }
     val minYValue by remember { derivedStateOf { minValue(data.minYValue(), maxYValue, yAxisConfig.labels.minValueAdjustment) } }
     val yRange by remember { derivedStateOf { range(minYValue, maxYValue, yAxisConfig.labels.rangeAdjustment) } }
+
+    println("First X: ${firstXValue}, Last X: $lastXValue")
 
     var yLabels by remember { mutableStateOf(listOf(0f, 100f)) }
     try {
@@ -43,7 +60,27 @@ fun LineGraph(
         )
     } catch (e: InvalidRangeException) { state = Error }
 
-    Canvas(modifier = modifier) {
+    val xAxisPosition = xAxisConfig.axisLine.axisPosition?.toXAxisPosition() ?: when {
+        maxYValue <= 0 -> XAxisPosition.TOP
+        minYValue < 0 -> XAxisPosition.CENTER
+        else -> XAxisPosition.BOTTOM
+    }
 
+    val yAxisPosition = yAxisConfig.axisLine.axisPosition?.toYAxisPosition() ?: YAxisPosition.START
+
+    Canvas(modifier = modifier) {
+        if (yAxisConfig.showAxis) {
+            continuousYAxis(
+                config = yAxisConfig,
+                labels = yLabels,
+                range = yRange,
+                yRangeValues = Range(min = minYValue, max = maxYValue),
+                yAxisPosition = yAxisPosition,
+                xAxisPosition = xAxisPosition,
+                drawXAxis = false,
+                drawZero = true,
+                textMeasurer = yTextMeasurer
+            )
+        }
     }
 }
