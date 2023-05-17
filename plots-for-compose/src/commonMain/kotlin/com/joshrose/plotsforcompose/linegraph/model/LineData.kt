c0d3d3f368315343c847619cb28cef575c9924c6
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-17 20:24:10 +0300

    Works around Data Class not being overrideable; LineData is now a sealed class and takes a generic property that is inherited by its member data classes.

--- plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/linegraph/model/LineData.kt
+++ plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/linegraph/model/LineData.kt
@@ -3,10 +3,20 @@ package com.joshrose.plotsforcompose.linegraph.model
 import com.arkivanov.essenty.parcelable.Parcelable
 import com.arkivanov.essenty.parcelable.Parcelize
 
-@Parcelize
-data class LineData(val x: Any?, val y: Float): Parcelable
+sealed class LineData<T: Comparable<T>>(open val x: T, open val y: Float): Parcelable {
+    @Parcelize
+    data class FloatData(override val x: Float, override val y: Float): LineData<Float>(x, y)
 
-fun List<LineData>.lastXValue() = last().x
-fun List<LineData>.maxYValue() = maxOf { it.y }
-fun List<LineData>.firstXValue() = first().x
-fun List<LineData>.minYValue() = minOf { it.y }
+    @Parcelize
+    data class IntData(override val x: Int, override val y: Float): LineData<Int>(x, y)
+
+    @Parcelize
+    data class StringData(override val x: String, override val y: Float): LineData<String>(x, y)
+
+    companion object {
+        fun List<LineData<*>>.lastXValue() = last().x
+        fun List<LineData<*>>.maxYValue() = maxOf { it.y }
+        fun List<LineData<*>>.firstXValue() = first().x
+        fun List<LineData<*>>.minYValue() = minOf { it.y }
+    }
+}
