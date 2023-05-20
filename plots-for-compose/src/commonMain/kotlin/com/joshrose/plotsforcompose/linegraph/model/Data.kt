7093df1a060c216976216ea6414d5400bd0a3b5e
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-20 12:30:43 +0300

    Moves extensions functions to companion object.

--- plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/linegraph/model/Data.kt
+++ plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/linegraph/model/Data.kt
@@ -4,17 +4,22 @@ import com.arkivanov.essenty.parcelable.Parcelable
 import com.arkivanov.essenty.parcelable.Parcelize
 
 @Parcelize
-data class NumberData(val x: Number, val y: Float): Parcelable
-
-fun List<NumberData>.maxXValue() = maxOf { it.x.toFloat() }
-fun List<NumberData>.maxYValue() = maxOf { it.y }
-fun List<NumberData>.minXValue() = minOf { it.x.toFloat() }
-fun List<NumberData>.minYValue() = minOf { it.y }
+data class NumberData(val x: Number, val y: Float): Parcelable {
+    companion object {
+        fun List<NumberData>.maxXValue() = maxOf { it.x.toFloat() }
+        fun List<NumberData>.maxYValue() = maxOf { it.y }
+        fun List<NumberData>.minXValue() = minOf { it.x.toFloat() }
+        fun List<NumberData>.minYValue() = minOf { it.y }
+    }
+}
 
 @Parcelize
-data class StringData(val x: String, val y: Float): Parcelable
+data class StringData(val x: String, val y: Float): Parcelable {
+    companion object {
+        fun List<StringData>.firstXValue() = first().x
+        fun List<StringData>.lastXValue() = last().x
+        fun List<StringData>.minYValue() = minOf { it.y }
+        fun List<StringData>.maxYValue() = maxOf { it.y }
+    }
+}
 
-fun List<StringData>.firstXValue() = first().x
-fun List<StringData>.lastXValue() = last().x
-fun List<StringData>.minYValue() = minOf { it.y }
-fun List<StringData>.maxYValue() = maxOf { it.y }
