c52162188634005a4e01030e7554139719c2e163
ubertrombone <josh_rose@alumni.baylor.edu>
2023-05-22 12:10:04 +0300

    Makes Range parameter covariant.

--- plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/axis/util/Range.kt
+++ plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/axis/util/Range.kt
@@ -1,6 +1,6 @@
 package com.joshrose.plotsforcompose.axis.util
 
-data class Range<T>(val min: T, val max: T) {
+data class Range<out T>(val min: T, val max: T) {
     companion object {
         fun <T: Number> Range<T>.mapToFloat() = Range(min = min.toFloat(), max = max.toFloat())
     }
