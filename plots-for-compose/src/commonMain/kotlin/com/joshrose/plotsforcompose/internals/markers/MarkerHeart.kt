d1d8256582a82f59a7d5d277a59509b5fe1e22a3
ubertrombone <josh_rose@alumni.baylor.edu>
2023-08-16 20:31:14 +0300

    Converts markerHeart to a path only drawing.

--- plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/internals/markers/MarkerHeart.kt
+++ plots-for-compose/src/commonMain/kotlin/com/joshrose/plotsforcompose/internals/markers/MarkerHeart.kt
@@ -1,10 +1,11 @@
 package com.joshrose.plotsforcompose.internals.markers
 
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Fill
 import com.joshrose.plotsforcompose.internals.plots.lines.MarkerProperties
 import com.joshrose.plotsforcompose.internals.util.toRadians
 import kotlin.math.sin
@@ -21,31 +22,38 @@ internal fun DrawScope.markerHeart(markerProperties: MarkerProperties) {
     val leftPoint = Offset(x = markerProperties.coordinates.first.minus(topPointXOffset), y = markerProperties.coordinates.second.minus(radius))
     val rightPoint = Offset(x = markerProperties.coordinates.first.plus(topPointXOffset), y = markerProperties.coordinates.second.minus(radius))
 
+    val circleYCenterOffset = radius.times(sin(30f.toRadians()))
+    val circleCenterY = leftPoint.y.minus(circleYCenterOffset)
+
     val path = Path().apply {
         moveTo(x = bottomPoint.x, y = bottomPoint.y)
         lineTo(x = leftPoint.x, y = leftPoint.y)
         lineTo(x = rightPoint.x, y = rightPoint.y)
         close()
+
+        addArc(
+            oval = Rect(
+                center = Offset(x = markerProperties.coordinates.first.minus(radius), y = circleCenterY),
+                radius = radius
+            ),
+            startAngleDegrees = 0f,
+            sweepAngleDegrees = 360f
+        )
+
+        addArc(
+            oval = Rect(
+                center = Offset(x = markerProperties.coordinates.first.plus(radius), y = circleCenterY),
+                radius = radius
+            ),
+            startAngleDegrees = 0f,
+            sweepAngleDegrees = 360f
+        )
+
+        moveTo(x = markerProperties.coordinates.first.minus(topPointXOffset), y = circleCenterY)
+        lineTo(x = markerProperties.coordinates.first.plus(topPointXOffset), y = circleCenterY)
+        lineTo(x = markerProperties.coordinates.first.plus(topPointXOffset), y = circleCenterY.plus(circleYCenterOffset))
+        lineTo(x = markerProperties.coordinates.first.minus(topPointXOffset), y = circleCenterY.plus(circleYCenterOffset))
+        close()
     }
-    drawPath(path = path, color = markerProperties.color ?: Color.White)
-
-    val circleYCenterOffset = radius.times(sin(30f.toRadians()))
-    val circleCenterY = leftPoint.y.minus(circleYCenterOffset)
-
-    drawCircle(
-        color = markerProperties.color ?: Color.White,
-        radius = radius,
-        center = Offset(x = markerProperties.coordinates.first.minus(radius), y = circleCenterY)
-    )
-    drawCircle(
-        color = markerProperties.color ?: Color.White,
-        radius = radius,
-        center = Offset(x = markerProperties.coordinates.first.plus(radius), y = circleCenterY)
-    )
-
-    drawRect(
-        color = markerProperties.color ?: Color.White,
-        topLeft = Offset(x = markerProperties.coordinates.first.minus(topPointXOffset), y = circleCenterY),
-        size = Size(width = topPointXOffset.times(2f), height = circleYCenterOffset)
-    )
+    drawPath(path = path, color = markerProperties.color ?: Color.White, style = Fill)
 }
